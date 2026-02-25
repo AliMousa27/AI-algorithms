255e19ed1675fc7e933687918a726aee9c155df3
AliMousa27 <jafar.myhero@gmail.com>
2026-02-25 17:31:34 +0100

    finish ass2

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
+from sklearn.metrics.pairwise import euclidean_distances
+from collections import Counter
+import numpy as np
+
+
 from sklearn.datasets import make_gaussian_quantiles
 import numpy as np
 from matplotlib import pyplot as plt
 import math
+# Randomly generate data from two different distributions
 data_class1, _ = make_gaussian_quantiles(mean = [1,1], cov = 1.5, n_samples = 30, n_features=2, random_state=18)
 data_class2, _ = make_gaussian_quantiles(mean = [-1,-1], cov= 0.75,n_samples = 8, n_features=2, random_state=4)
 
@@ -44,116 +50,157 @@ def decision_boundary(model, fig, ax, levels = [0.1,0.5,0.9], labels = True):
         ax.clabel(contours, inline=1, fontsize=10)
 
     return fig, ax
-  
-class GaussianNaiveBayes():
+class LogisticRegression():
     """
-    Custom implementation of a Gaussian Naive Bayes classifier.
-    The parameters are estimated using MLE.
+    Custom implementation of (binary) Logistic Regression.
     """
-
-    def __init__(self):
+    def __init__(self, learning_rate, n_iterations, print_cost=False):
         """
         Parameter(s):
+            learning_rate - Learning rate
+            n_iterations  - Number of iterations
         """
-        self.mean = {}
-        self.std = {}
-        self.prior = {}
-        self.unique_classes = 0
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.print_cost = print_cost
+        self.parameters = {}
 
-    def get_class_parameters(self, X_class):
+    def initialize_parameters(self, n_features):
         """
-        Estimating the MLE of the parameters.
+        Initialize model parameters with zeros:
+            w.shape = (n_features,)
+            b.shape = (1,)
         --------------------
         Input(s):
-            X_class - Data points corresponding to a single class
+            n_features - Number of features
         --------------------
         Output(s):
-            mean_MLE - MLE of mean
-            std_MLE  - MLE of scale
         """
 
-        mean_MLE = np.mean(X_class, axis=0)
-        std_MLE = np.std(X_class, axis=0)
+        w = np.zeros(shape = (n_features,1))
+        b = 0
 
-        return mean_MLE, std_MLE
+        self.parameters['w'] = w
+        self.parameters['b'] = b
+
+    def sigmoid(self, z):
+        """
+        Sigmoid function
+        --------------------
+        Input(s):
+            z -
+        --------------------
+        Output(s):
+            s - sigmoid(z)
+        """
+        s = 1 / (1 + np.exp(-z))
+        return s
+
+    def grad_cost(self, X_train, y_train, w, b):
+        """
+        Computes the cost function (negative log-likelihood) and
+        partial derivatives of cost w.r.t the model parameters.
+        --------------------
+        Input(s):
+            X_train - Data of size(n_samples, n_features)
+            y_train - True labels of size (n_samples,1)
+            w,b     - Model parameters
+        --------------------
+        Output(s):
+            cost  - Negative log-likelihood cost
+            grads - Gradients of loss function w.r.t model parameters (dw,db).
+        """
+        N = X_train.shape[0]
+
+        y_train = y_train.reshape(-1,1)
+
+        ### TODO - Change this part
+        # Dummy variables currently
+        A =  self.sigmoid(np.add(X_train @ w , b))
+        cost = -1/N * sum([(y_train[n] * np.log(A[n]) + (1 - y_train[n])*np.log(1 - A[n])) for n in range(N)])
+        dw = 1/N * X_train.T @ (A-y_train)
+        db = 1/N * sum([(A[n] - y_train[n]) for n in range(N)])
+        ###
+        grads = {"dw": dw,
+                 "db": db}
+        return cost, grads
 
     def fit(self, X_train, y_train):
         """
-        Compute model parameters using maximum likelihood estimates and a class size prior.
+        Optimize model parameters by running a gradient descent algorithm.
         --------------------
         Input(s):
-            X_train   - Data of size (n_samples, n_features)
-            y_train   - True labels of size (n_samples,1)
+            X_train - Data of size (n_samples, n_features)
+            y_train - True labels of size (n_samples,1)
         --------------------
         Output(s)
         """
+        n_features = X_train.shape[1]
 
-        # Compute mean, variance and prior of each class
-        self.unique_classes = np.unique(y_train)
-        for uc in self.unique_classes:
-            X_class = X_train[y_train == uc]
-            c_mean, c_std = self.get_class_parameters(X_class)
-            self.mean[uc] = c_mean
-            self.std[uc] = c_std
-            self.prior[uc] = X_class.shape[0]/X_train.shape[0]
+        # Initialize parameters
+        self.params = self.initialize_parameters(n_features)
 
-    def gaussian_density(self, x, mu, std):
-        """
-        1-D Gaussian density function.
-        --------------------
-        Input(s):
-            x   - Data point
-            mu  - mean
-            std - scale
-        --------------------
-        Output(s):-
-            N(mu, std^2)
+        # Gradient descent
+        w = self.parameters["w"]
+        b = self.parameters["b"]
+        for i in range(1,self.n_iterations+1):
+            cost, grads = self.grad_cost(X_train, y_train, w, b)
+            dw = grads["dw"]
+            db = grads["db"]
 
-        """
-        return 1/(std*np.sqrt(2*np.pi))*np.exp(-(1/2)*((x-mu)/std)**2)
+            w -= self.learning_rate * dw
+            b -= self.learning_rate * db
+
+            if self.print_cost and i % 100 == 0:
+                print(f"Cost after iteration {i}: {cost}")
+
+        self.parameters = {"w": w,
+                           "b": b}
+
+        return self
 
     def predict(self, X_test):
         """
-        Prediction of test data.
+        Binary prediction of test data.
         --------------------
         Input(s):
-            X_test   - Data of size (n_samples, n_features)
+            X   - Data of size (n_samples, n_features)
         --------------------
         Output(s)
-            y_pred - Predicted labels of size (n_samples,)
-            y_pred_prob - Probabilistic labels of size (n_samples,n_classes)
+            y_pred - Predicted labels (0/1) of size (n_samples,)
         """
 
-        n_samples = X_test.shape[0]
-        y_pred_prob = np.zeros(shape=(n_samples,len(self.unique_classes)), dtype=np.float32)
+        w = self.parameters["w"]
+        b = self.parameters["b"]
+
+        N = X_test.shape[0]
+        y_pred = np.zeros((N,1))
 
         ### TODO - Change this part
-        for i in range(n_samples):
-            # we need to output [1,0] [0,1] [0.5,0.5]
-            y_pred_prob[i,:] = [0] * len(self.unique_classes)
-            for c in self.unique_classes:
-              sample = X_test[i]
-              top = math.prod([self.gaussian_density(xnd,self.mean[c][d],self.std[c][d]) for d,xnd in enumerate(sample)]) * self.prior[c]
-              bottom = sum(
-                math.prod([self.gaussian_density(xnd, self.mean[cPrime][d], self.std[cPrime][d]) for d,xnd in enumerate(sample)]) * self.prior[cPrime]
-                          for cPrime in self.unique_classes)
-              p = top/bottom
-              y_pred_prob[i,c] = p
-            #y_pred_prob[i,:] = [1,0] # Always predicts class 0 currently
+        A = self.sigmoid(X_test @ w + b)
+        r=0.5
+        y_pred = (A >= r).astype(int)
+        ###
 
-        y_pred = np.argmax(y_pred_prob, axis=-1)
-        return y_pred, y_pred_prob
+        return y_pred.squeeze(), A
+    
+
+# Parameters (feel free to change)
+learning_rate = 0.01
+n_iterations = 1000
+print_cost = True
 
 # Define and train the model
-model = GaussianNaiveBayes()
+model = LogisticRegression(learning_rate, n_iterations, print_cost)
 model.fit(X_train, y_train)
 
-# Prediction
+# Predictions
 y_pred_train, _ = model.predict(X_train)
 print("Train accuracy: %.3f %%" %(np.mean(y_pred_train == y_train)*100))
 y_pred_test,y_pred_prob_test = model.predict(X_test)
-print(f"Predicted test class: ${y_pred_test[0]}, (prob ${y_pred_prob_test[0,y_pred_test]})")
+#printing the entire probabilty. if we do 0.2f we got 0.56
+print(f"Predicted test class: {y_pred_test}, {np.where(y_pred_prob_test < 0.5, 1-y_pred_prob_test, y_pred_prob_test)}")
 
 # Plot decision-boundaries
-fig, ax = plot_data(X_train, y_train, X_test, title = 'Lines showing $P(y_{n} = 0 | x_{n}, X, y)$')
+fig, ax = plot_data(X_train, y_train, X_test, title = 'Lines showing $P(y_{n} = 1 | x_{n}, X, y)$')
 fig, ax = decision_boundary(model, fig, ax, labels = True)
